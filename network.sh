dcc7b0dc044db0909565ea143ea00a758b2f3ccf
NguyenHNhan <115022604+NguyenHNhan@users.noreply.github.com>
2023-09-29 08:53:03 +0700

    Create network.sh

---
+++ network.sh
@@ -0,0 +1,3 @@
+ apt -y install neutron-plugin-ml2 neutron-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent python3-neutronclient 
+
+ /etc/neutron/neutron.conf https://github.com/NguyenHNhan/Openstack/raw/main/1963d8a2a4e42ad8816d9359da211e86eb2e513f/conf/network/neutron.conf
