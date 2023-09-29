56b5a552dcc8e4dc87e57370242167c39be99d42
NguyenHNhan <115022604+NguyenHNhan@users.noreply.github.com>
2023-09-29 21:18:23 +0700

    Create password.sh

---
+++ password.sh
@@ -0,0 +1,9 @@
+#MAT KHAU ROOT MYSQL
+MARIAPASS="123"
+######################
+#MAT KHAU USER SU DUNG MYSQL
+USERDB="123"
+######################
+#MAT KHAU USER OPENSTACK
+UOPENSTACK="123"
+######################
