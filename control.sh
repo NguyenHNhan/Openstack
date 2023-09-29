329924cd216817a2defbc08405ddda8832388458
NguyenHNhan <115022604+NguyenHNhan@users.noreply.github.com>
2023-09-29 22:57:52 +0700

    Update control.sh

--- control.sh
+++ control.sh
@@ -116,7 +116,9 @@ openstack role add --project service --user neutron admin
 openstack service create --name neutron --description "OpenStack Networking service" network 
 openstack endpoint create --region RegionOne network public http://$IPControl:9696 
 
-apt -y install neutron-server neutron-plugin-ml2 neutron-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent python3-neutronclient 
+apt -y install neutron-server 
+
+#neutron-plugin-ml2 neutron-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent python3-neutronclient 
 
 mv /etc/neutron/neutron.conf /etc/neutron/neutron.org
 wget -O  /etc/neutron/neutron.conf https://github/NguyenHNhan/Openstack/raw/main/conf/neutron.conf
@@ -125,17 +127,17 @@ touch /etc/neutron/fwaas_driver.ini
 chmod 640 /etc/neutron/{neutron.conf,fwaas_driver.ini} 
 chgrp neutron /etc/neutron/{neutron.conf,fwaas_driver.ini} 
 
-mv /etc/neutron/l3_agent.ini /etc/neutron/l3_agent.org
-wget -O /etc/neutron/l3_agent.ini https://github/NguyenHNhan/Openstack/raw/main/conf/l3_agent.ini
+#mv /etc/neutron/l3_agent.ini /etc/neutron/l3_agent.org
+#wget -O /etc/neutron/l3_agent.ini https://github/NguyenHNhan/Openstack/raw/main/conf/l3_agent.ini
 
-mv /etc/neutron/dhcp_agent.ini /etc/neutron/dhcp_agent.org
-wget -O /etc/neutron/dhcp_agent.ini https://github/NguyenHNhan/Openstack/raw/main/conf/dhcp_agent.ini
+#mv /etc/neutron/dhcp_agent.ini /etc/neutron/dhcp_agent.org
+#wget -O /etc/neutron/dhcp_agent.ini https://github/NguyenHNhan/Openstack/raw/main/conf/dhcp_agent.ini
 
-mv /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugins/ml2/ml2_conf.org
-wget -O /etc/neutron/plugins/ml2/ml2_conf.ini 
+#mv /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugins/ml2/ml2_conf.org
+#wget -O /etc/neutron/plugins/ml2/ml2_conf.ini 
 
-mv /etc/neutron/plugins/ml2/linuxbridge_agent.ini /etc/neutron/plugins/ml2/linuxbridge_agent.org
-/etc/neutron/plugins/ml2/linuxbridge_agent.ini 
+#mv /etc/neutron/plugins/ml2/linuxbridge_agent.ini /etc/neutron/plugins/ml2/linuxbridge_agent.org
+#/etc/neutron/plugins/ml2/linuxbridge_agent.ini 
 
 /etc/nova/nova.conf 
 apt -y install openstack-dashboard 
