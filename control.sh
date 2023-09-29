45c70fcc1651af01bee18a1768dcc0f3626c14d5
NguyenHNhan <115022604+NguyenHNhan@users.noreply.github.com>
2023-09-29 21:56:01 +0700

    Update control.sh

--- control.sh
+++ control.sh
@@ -30,10 +30,10 @@ wget -O /etc/keystone/keystone.conf https://github.com/NguyenHNhan/Openstack/raw
 su -s /bin/bash keystone -c "keystone-manage db_sync" 
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone 
 keystone-manage credential_setup --keystone-user keystone --keystone-group keystone 
-keystone-manage bootstrap --bootstrap-password 123 \
---bootstrap-admin-url http://172.20.200.7:5000/v3/ \
---bootstrap-internal-url http://172.20.200.7:5000/v3/ \
---bootstrap-public-url http://172.20.200.7:5000/v3/ \
+keystone-manage bootstrap --bootstrap-password $UOPENSTACK \
+--bootstrap-admin-url http://$IPControl:5000/v3/ \
+--bootstrap-internal-url http://$IPControl:5000/v3/ \
+--bootstrap-public-url http://$IPControl:5000/v3/ \
 --bootstrap-region-id RegionOne
 
 mv /etc/apache2/apache2.conf /etc/apache2/apache2.org
@@ -49,8 +49,8 @@ export OS_PROJECT_DOMAIN_NAME=default
 export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=admin
 export OS_USERNAME=admin
-export OS_PASSWORD=123
-export OS_AUTH_URL=http://172.20.200.7:5000/v3
+export OS_PASSWORD=$UOPENSTACK
+export OS_AUTH_URL=http://$IPControl:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
 
@@ -59,7 +59,7 @@ openstack project create --domain default --description "Service Project" servic
 openstack user create --domain default --project service --password $UOPENSTACK glance 
 openstack role add --project service --user glance admin 
 openstack service create --name glance --description "OpenStack Image service" image
-openstack endpoint create --region RegionOne image public http://172.20.200.7:9292
+openstack endpoint create --region RegionOne image public http://$IPControl:9292
 
 
 
@@ -78,8 +78,8 @@ openstack user create --domain default --project service --password $UOPENSTACK 
 openstack role add --project service --user placement admin 
 openstack service create --name nova --description "OpenStack Compute service" compute 
 openstack service create --name placement --description "OpenStack Compute Placement service" placement 
-openstack endpoint create --region RegionOne compute public http://172.20.200.7:8774/v2.1/%\(tenant_id\)s 
-openstack endpoint create --region RegionOne placement public http://172.20.200.7:8778 
+openstack endpoint create --region RegionOne compute public http://$IPControl:8774/v2.1/%\(tenant_id\)s 
+openstack endpoint create --region RegionOne placement public http://$IPControl:8778 
 
 apt -y install nova-api nova-conductor nova-scheduler nova-novncproxy placement-api python3-novaclient 
 ""
@@ -111,7 +111,7 @@ openstack compute service list
 openstack user create --domain default --project service --password $UOPENSTACK neutron 
 openstack role add --project service --user neutron admin 
 openstack service create --name neutron --description "OpenStack Networking service" network 
-openstack endpoint create --region RegionOne network public http://172.20.200.7:9696 
+openstack endpoint create --region RegionOne network public http://$IPControl:9696 
 
 apt -y install neutron-server neutron-plugin-ml2 neutron-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent python3-neutronclient 
 
