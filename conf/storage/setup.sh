f83fe7a6372aa2371f4d48c0e2a3435e326ba462
NguyenHNhan <115022604+NguyenHNhan@users.noreply.github.com>
2023-10-01 12:30:38 +0700

    Update setup.sh

--- conf/storage/setup.sh
+++ conf/storage/setup.sh
@@ -1,3 +1,8 @@
+apt -y install cinder-volume python3-mysqldb
+
+mv /etc/cinder/cinder.conf /etc/cinder/cinder.conf.org
+wget -O /etc/cinder/cinder.conf 
+
 chmod 640 /etc/cinder/cinder.conf
 chgrp cinder /etc/cinder/cinder.conf
 systemctl restart cinder-volume
