503621260cc0927121655ed4307e79ae1f80deea
NguyenHNhan <115022604+NguyenHNhan@users.noreply.github.com>
2023-10-01 12:31:55 +0700

    Update setup.sh

--- conf/storage/setup.sh
+++ conf/storage/setup.sh
@@ -1,7 +1,7 @@
 apt -y install cinder-volume python3-mysqldb
 
 mv /etc/cinder/cinder.conf /etc/cinder/cinder.conf.org
-wget -O /etc/cinder/cinder.conf 
+wget -O /etc/cinder/cinder.conf https://github.com/NguyenHNhan/Openstack/raw/main/conf/storage/conf/cinder.conf
 
 chmod 640 /etc/cinder/cinder.conf
 chgrp cinder /etc/cinder/cinder.conf
